5fc2582b4699d380f887e9ae4a7bbf56a95c8550
Peter <peter.s@webgroup-limited.com>
2019-10-15 11:54:24 +0200

    Added big-uploads site type and after.sh

--- resources/after.sh
+++ resources/after.sh
@@ -24,3 +24,6 @@
 # Install Node.js v10.x
 #curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 #sudo apt-get install -y nodejs
+
+sudo apt-get install -y php-memcache
+sudo apt install -y mc
